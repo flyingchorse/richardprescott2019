7371d75907471c8956052bddb704a38dbed5ae41
David Griffin <dave@digidol-media.net>
2019-11-19 22:09:35 +0000

    Footer

--- footer.php
+++ footer.php
@@ -16,7 +16,30 @@ $container = get_theme_mod( 'understrap_container_type' );
 <?php get_template_part( 'sidebar-templates/sidebar', 'footerfull' ); ?>
 
 <div class="wrapper" id="wrapper-footer">
-
+	<div class="container">
+	<div class="row">
+		<div class="col-md-4">RICHARD PRESCOTT PHOTOGRAPHER</div>
+		<div class="col-md-2">
+		  <ul class="navbar-nav">
+		  <li>HOME</li>
+		  <li>AUTOMOTIVE</li>
+		  <li>CGI</li>
+		  <li>LANDSCAPE</li>
+		  <li>MARINE</li>
+		  </ul>
+		</div>
+		<div class="col-md-2">
+		  <ul class="navbar-nav">
+		  <li>PORTRAIT</li>
+		  <li>MOVING IMAGE</li>
+		  <li>EDITIONS</li>
+		  <li>INFORMATION</li>
+		  <li></li>
+		  </ul>
+		 </div>
+		 <div class="col-md-2">Social</div>
+	</div> <!-- End of Row -->
+	</div>
 	<div class="<?php echo esc_attr( $container ); ?>">
 
 		<div class="row">
