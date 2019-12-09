4a54199a3159fe903ad9c630c282b2390371d22d
David Griffin <dave@digidol-media.net>
2019-12-09 23:08:06 +0000

    changed the footer to centred and adding in some padding

--- footer.php
+++ footer.php
@@ -17,24 +17,26 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 <div class="wrapper" id="wrapper-footer">
 	<div class="container">
-	<div class="row">
-		<div class="col-md-4 mb-4">RICHARD PRESCOTT PHOTOGRAPHER</div>
-		<div class="col-md-4">
+		<div class="row  justify-content-center pb-4">RICHARD PRESCOTT PHOTOGRAPHER</div>
+	<div class="row  justify-content-center">
+		
+	<nav class="navbar navbar-expand-md navbar-light justify-content-center pb-4">	
 		  <?php wp_nav_menu(
 					array(
 						'theme_location'  => 'primary',
-						'container_class' => 'footer-nav',
-						'container_id'    => 'footerNav',
-						'menu_class'      => 'navbar-nav footer-nav-menu',
+						'container_class' => 'navbar-collapse justify-content-center',
+						'container_id'    => 'navbarNavDropdown',
+						'menu_class'      => 'navbar-nav',
 						'fallback_cb'     => '',
 						'menu_id'         => 'main-menu',
 						'depth'           => 3,
 						'walker'          => new Understrap_WP_Bootstrap_Navwalker(),
 					)
-				); ?>		</div>
-		 <div class="col-md-2"><a class="d-inline-flex" href="https://www.instagram.com/richard_prescott/"><img  src="<?php echo get_stylesheet_directory_uri(  ); ?>/src/img/IG_Glyph_Fill.png" alt="Instagram" width="70" height="70" /></a>
-		 <a class="d-inline-flex" href="https://vimeo.com/user17645474"><img class="d-inline-flex" src="<?php echo get_stylesheet_directory_uri(  ); ?>/src/img/vimeo_icon_dark.png" alt="vimeo_icon_dark" width="70" height="70" /></a></div>
-	</div> <!-- End of Row -->
+				); ?>		
+	</nav>		 
+	</div><!-- End of Row -->
+	<div class="row  justify-content-center pb-4"><a class="d-inline-flex" href="https://www.instagram.com/richard_prescott/"><img  src="<?php echo get_stylesheet_directory_uri(  ); ?>/src/img/IG_Glyph_Fill.png" alt="Instagram" width="70" height="70" /></a></div>
+	<div class="row  justify-content-center"><a class="d-inline-flex" href="https://vimeo.com/user17645474"><img class="d-inline-flex" src="<?php echo get_stylesheet_directory_uri(  ); ?>/src/img/vimeo_icon_dark.png" alt="vimeo_icon_dark" width="70" height="70" /></a></div> 
 	</div>
 	
 
