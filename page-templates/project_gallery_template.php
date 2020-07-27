0a9768936093ea4333913c5965fa2112fffbf7f0
David Griffin <dave@digidol-media.net>
2020-07-27 14:29:56 +0100

    UPdated standard gallery template

--- page-templates/project_gallery_template.php
+++ page-templates/project_gallery_template.php
@@ -16,6 +16,6 @@ $sidebar_pos = get_theme_mod( 'understrap_sidebar_position' );
 // is active and the current page is a WooCommerce page and we do
 ?>
 
-<?php digidol_hero();?>
+<?php digidol_gallery_column_layout();?>
 
 <?php get_footer(); ?>
