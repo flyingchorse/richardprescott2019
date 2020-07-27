402c02cc1a22b28bcc4def085aaf616ce75b5e52
David Griffin <dave@digidol-media.net>
2020-07-27 12:02:57 +0100

    added new template

---
+++ page-templates/project_gallery_column.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Template Name: Project Column Layout
+ * Template Post Type: post, page, event, projects
+ * Template for displaying a page without sidebar even if a sidebar widget is published.
+ *
+ * @package understrap
+ */
+
+get_header('automotive');
+
+$container   = get_theme_mod( 'understrap_container_type' );
+$sidebar_pos = get_theme_mod( 'understrap_sidebar_position' );
+// On WooCommerce pages there is no need for sidebars as they leave
+// too little space for WooCommerce itself. We check if WooCommerce
+// is active and the current page is a WooCommerce page and we do
+?>
+
+<?php digidol_hero();?>
+
+<?php get_footer(); ?>
