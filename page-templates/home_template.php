3fb6eeb1d5570b671e3ee27c15f300f304e389de
flyingchorse <dave@digidol-media.net>
2021-08-27 13:18:47 +0100

    Maintained fullscreen layout and moved to a home

--- page-templates/home_template.php
+++ page-templates/home_template.php
@@ -7,7 +7,7 @@
  * @package understrap
  */
 
-get_header(landingpage);
+get_header(home);
 
 $container   = get_theme_mod( 'understrap_container_type' );
 $sidebar_pos = get_theme_mod( 'understrap_sidebar_position' );
