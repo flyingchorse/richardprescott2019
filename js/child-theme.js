f74c9e775c885ddf640f4d805f45d76d73f12b4f
flyingchorse <dave@digidol-media.net>
2022-12-08 22:52:54 +0000

    cookie control

--- js/child-theme.js
+++ js/child-theme.js
@@ -7117,8 +7117,37 @@ function showPage() {
 //document.onselectstart =new Function("return false;")
 
 
-const mainNode = document.getElementById('eu-cookie-law');
+function getCookie(name) {
+  var dc = document.cookie;
+  var prefix = name + "=";
+  var begin = dc.indexOf("; " + prefix);
+
+  if (begin == -1) {
+    begin = dc.indexOf(prefix);
+    if (begin != 0) return null;
+  } else {
+    begin += 2;
+    var end = document.cookie.indexOf(";", begin);
+
+    if (end == -1) {
+      end = dc.length;
+    }
+  } // because unescape has been deprecated, replaced with decodeURI
+  //return unescape(dc.substring(begin + prefix.length, end));
+
+
+  return decodeURI(dc.substring(begin + prefix.length, end));
+}
+
+const mainNode = document.getElementById("eu-cookie-law");
 var hamburg = document.getElementById("eu_cookie_law_widget-2");
+var cookiesalreadyaccept = document.getElementById("eu-cookie-law");
+var myCookie = getCookie("eucookielaw");
+
+if (myCookie == null) {// do cookie doesn't exist stuff;
+} else {
+  hamburg.classList.add("cookiemodal-accepted"); // do cookie exists stuff
+}
 
 function callback(mutationsList, observer) {
   console.log('Mutations:', mutationsList);
